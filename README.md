eee03a88f63a68ba10726297515a25915c9de44c
deepsafedev <deepsafedev@gmail.com>
2026-05-14 18:02:08 +0800

    chore: rename to DeepS

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SafeX docs
+# DeepS docs
 
 This is a Next.js application generated with
 [Create Fumadocs](https://github.com/fuma-nama/fumadocs).
--- lib/shared.ts
+++ lib/shared.ts
@@ -1,11 +1,11 @@
-export const appName = 'SafeX Docs';
+export const appName = 'DeepS Docs';
 export const docsRoute = '/';
 export const docsImageRoute = '/og';
 export const docsContentRoute = '/llms.mdx';
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'safexn',
-  repo: 'safex-docs',
+  user: 'deepslabs',
+  repo: 'deeps-docs',
   branch: 'master',
 };
